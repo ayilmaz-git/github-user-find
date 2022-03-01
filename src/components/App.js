8cb3e1f557ebbefab86151eeff42e500bc897966
aziz atilla yılmaz <atilla.yilmaz@ogr.iu.edu.tr>
2022-03-01 05:38:25 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
+import Alert from './Alert'
+import About from './About'
+import NotFound from './NotFound'
+import UserDetails from './UserDetails'
+import GithubState from '../context/github/githubState'
+import AlertState from '../context/alert/alertState'
+
+const App = () => {
+
+    
+    return (
+        <GithubState>
+            <AlertState>
+                    <BrowserRouter>
+                                <Navbar />
+                                <Alert />
+                                <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route path="/about" component={About} />
+                                <Route path="/user/:login" component={UserDetails}/>
+                                <Route component={NotFound}/>
+                                </Switch>
+                </BrowserRouter>
+            </AlertState>
+        </GithubState>                
+    )
+  
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "./Search";
+import Users from "./Users";
+
+
+const Home = () => {
+    return (
+        <>
+            <Search/>
+            <Users/>
+        </>
+    )
+}
+
+export default Home
